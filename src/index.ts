fd31a2fe99a4d4b72dabd899613062ed019ef391
MCCTUA <mcctua@gmail.com>
2025-05-22 16:59:57 +0700

    setting up cors, morgan, path, static path

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import express, {Request, Response} from "express"
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "node:path";
+
+const app = express();
+app.use(cors()); // allow all origin
+app.use(morgan("dev"));
+app.use(express.json({ limit: "50mb" }));
 
 // http://localhost:4000/api
 app.use("/api", (_req: Request, res: Response) => {
-    res.json({message: "Hello World"})
-})
+  res.json({ message: "Hello World" });
+});
 
-app.listen(4000, () => {
-    console.log('Server running on port 4000')
-})
+//http://localhost:4000/file_name
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server running on port ${process.env.PORT} || ENV: ${process.env.NODE_ENV}`
+  );
+});
